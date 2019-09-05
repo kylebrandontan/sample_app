eee350121459492ab7731120f09b8c17ad381c00
KYLE <kylebrandontan@gmail.com>
2019-09-05 14:06:13 +0800

    change title

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title, "RoR Tutorial App"
-    assert_equal full_title("Help"), "Help | RoR Tutorial App"
+    assert_equal full_title, "Twitter Clone"
+    assert_equal full_title("Help"), "Help | Twitter Clone"
   end
 end
